102b72d70db304d7d798848828c184f1054f8393
SeanMoody <seanmoodyeducation@gmail.com>
2023-04-17 15:04:04 -0400

    Added methods to user.rb

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,8 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
 	validates :username, :session_token, presence: true, uniqueness: true
 	validates :password_digest, presence: true
 	# validates :session_token, presence: true, uniqueness: true
 	before_validation :ensure_session_token
+	attr_reader :password
+
+	def self.find_by_credentials(username, password)
+
+		user = User.find_by(username: username) #first username is column second is argument
+
+		if user && user.is_password?(password)
+			user
+		else
+			nil
+		end
+	end
 
 	def password=(password)
 		self.password_digest = BCrypt::Password.create(password)
@@ -14,5 +37,16 @@ class User < ApplicationRecord
 		password_object.is_password?(password)
 	end
 
+	def reset_session_token
+		self.session_token = SecureRandom::urlsafe_base64
+		self.save!
+		self.session_token
+	end
+
+	private
+	
+	def generate_unique_session_token
+		self.session_token ||= SecureRandom::urlsafe_base64
+	end
 	
 end
