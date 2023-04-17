34e5300158b59c50f3f0eb5f6d2b6407f8571bb4
carveyh <carvey.hor@gmail.com>
2023-04-17 17:31:10 -0400

    SessionsController#create - complete, need testing

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -6,12 +6,27 @@ class SessionsController < ApplicationController
     end
 
     def create
-        # @user = User.new
-        render :root
+        #verify username & password
+        username = params[user][:username]
+        password = params[user][:password]
+        @user = User.find_by_credentials(username, password) #either user or nil
+
+        if @user
+            #reset user's session token
+            #update session's session token
+            login!(@user)
+            #redirect to cats index page
+            redirect_to root_url
+        else
+            redirect_to new_session_url
+        end
+
+        
+
     end
 
     def destroy
-
+        #do later
     end
 
 end
